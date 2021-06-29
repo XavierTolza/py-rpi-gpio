c003fd3d0c18dd7642881490c996d0183a2aec5d
Xavier Tolza <tolza.xavier@gmail.com>
2021-06-29 23:10:05 +0200

    wip: implemented fake gpio

--- gpio/controller.py
+++ gpio/controller.py
@@ -1,2 +1,17 @@
-from warnings import warn
-import RPi.GPIO as gpio
+from easylogger import LoggingClass
+
+try:
+    import RPi.GPIO as gpio
+except RuntimeError:
+    class FakeGPIO(LoggingClass):
+        IN = "IN"
+        OUT = "out"
+        FALLING = "falling"
+        RISING = "RISING"
+        BOARD = "BOARD"
+
+        def setmode(self, mode):
+            self.info(f"Set mode {mode}")
+
+
+    gpio = FakeGPIO()
